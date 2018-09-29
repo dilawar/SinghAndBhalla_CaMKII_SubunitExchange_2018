de3ad5a1d5b2710e8b018b44e1e6d8be98b937fa
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-29 15:59:27 +0530

    Normalized decay.

---
+++ PaperFigures/elifeFigure6/fit.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fit.py: 
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as sco
+import pandas as pd
+
+def split( tvec, vec, N = 7 ):
+    # Split where tvec is multiple of 500
+    splitAt = []
+    for i, t in enumerate(tvec):
+        if t % 500 == 0:
+            splitAt.append( i )
+
+    # Drop the last one
+    splitAt.pop()
+
+    ys = np.split( vec, splitAt )
+    minL = min( [len(x) for x in ys] )
+    # Ignore first 2 points from all trajectories since this is the time for
+    # which input is applied.
+    return [ y[2:minL] for y in ys ]
+
+def split_and_normalize( tvec, vec, N = 7 ):
+    ys = split( tvec, vec, N )
+    ys = [ y / y.max() if y.max() > 1 else y for y in ys ]
+    return ys
+
+def single_exp( t, tau, A ):
+    return A * np.exp( - t / tau )
+
+def find_midpoint( ys ):
+    maxY = ys.max()
+    for i, y in enumerate( ys ):
+        if y < maxY / 2:
+            return i
+    return -1
+
+def find_tau( ys ):
+    maxY = ys.max()
+    for i, y in enumerate( ys ):
+        if y < maxY * 0.63:
+            return i
+    return -1
+
+def estimate_nonzero_part( ys ):
+    for i, y in enumerate(ys):
+        if y <= 0.01:
+            return i
+    return len(ys)
+
+def main():
+    seOFF = pd.read_csv('./CaM100_PP90000_voxels20_suOFF.dat_processed.dat', sep = ' ')
+    seON = pd.read_csv('./CaM100_PP90000_voxels20_suON.dat_processed.dat', sep = ' ')
+
+    gridSize = (3, 2)
+    ax1 = plt.subplot2grid( gridSize, (0,0), colspan = 2 )
+    ax2 = plt.subplot2grid( gridSize, (1,0), colspan = 1 )
+    ax3 = plt.subplot2grid( gridSize, (1,1), colspan = 1 )
+    ax4 = plt.subplot2grid( gridSize, (2,0), colspan = 1 )
+    ax5 = plt.subplot2grid( gridSize, (2,1), colspan = 1 )
+    
+    seONCaMKII = seON['CaMKII*'].values
+    seOFFCaMKII = seOFF['CaMKII*'].values
+    seOFFt = seOFF['time'].values
+    seONt = seON['time'].values
+
+    # On this axis we plot the raw data.
+    ax1.plot( seONt, seONCaMKII, label = '+SE' )
+    ax1.plot( seOFFt, seOFFCaMKII, label = '-SE' )
+    ax1.legend()
+    ax1.set_xlabel( 'Time' )
+    ax1.set_ylabel( 'Activity' )
+
+    # Now split the trajectories at every 500 sec and take average.
+    # Drop the first input.
+    seONCaMKIISlices = split_and_normalize( seONt, seONCaMKII )
+    seOFFCaMKIISlices = split_and_normalize( seOFFt, seOFFCaMKII )
+    
+    dt = 5
+    avgY, stdY, ts = [], [], []
+    for label, ys in zip( ['+SE', '-SE'], [seONCaMKIISlices, seOFFCaMKIISlices]):
+        yerr = np.std(ys, axis=0)
+        y = np.mean(ys, axis=0)
+        avgY.append( y )
+        stdY.append( yerr )
+        t = np.arange(0, dt*(len(y)), dt)
+        ts.append(t)
+        try:
+            ax2.errorbar(t, y, yerr=yerr, label=label)
+        except Exception as e:
+            print( label, len(t), len(y), len(yerr) )
+            print( e )
+        #  ax2.set_xlim(0, 500)
+        ax2.legend()
+
+    ax2.set_title( 'Split at every 500 sec' )
+
+    # Fit curves.
+    for t, y, yerr, label, ax in zip(ts, avgY, stdY, ['+SE', '-SE'], [ax4,ax5]):
+        # Guess tau
+        tau0 = t[find_tau(y)]
+        print( f"[INFO ] init t0={tau0}" )
+        N = estimate_nonzero_part(y)
+        assert N > 1, N
+        try:
+            popt, pcov = sco.curve_fit( single_exp
+                    , t[:N], y[:N]
+                    , p0 = [tau0, y.max()]
+                    , sigma=yerr[:N]+1e-20
+                    , absolute_sigma=True
+                    #  , method = 'trf'
+                    )
+        except Exception as e:
+            print( e )
+            print( t )
+            print( y )
+            raise e
+        ax.set_title( r'$\tau$=%.2f,$A_{max}$=%.2f' % tuple(popt) )
+        ax.plot( t, single_exp(t, *popt), label = label)
+        ax.errorbar( t, y, yerr=yerr, alpha=0.5)
+        ax.legend()
+        print( popt )
+
+    # Add fitted curves.
+
+    plt.suptitle( 'Figure 6- eLife' )
+    plt.tight_layout( rect = (0,0,1,0.95) )
+    plt.savefig( '%s.pdf' % __file__ )
+
+
+if __name__ == '__main__':
+    main()
+
