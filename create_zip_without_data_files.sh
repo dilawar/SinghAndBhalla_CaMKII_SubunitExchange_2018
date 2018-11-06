a966f508a68754a4880e20065eeaa01600bee6f0
Dilawar Singh <dilawars@ncbs.res.in>
2018-11-06 12:14:47 +0530

    zip file with only essential figure files.

--- create_zip_without_data_files.sh
+++ create_zip_without_data_files.sh
@@ -5,7 +5,19 @@
 #    -o elife.zip
 # git clean -fx .
 OUTFILE=elife$(date +"%Y%m%d").zip
-zip -r $OUTFILE ./ -x ".git/*" -x "*.dat" -x "*.csv" -x ".git*" -x "\.*" \
-    -i "*.pdf" -i "*.tex" -i "*.cls" -i "*.sty" -i "*.bst" -i "*.cls" \
-    -i "*.bib"
+zip -r $OUTFILE \
+    ./CaMKII_Paper_2018-elife.tex \
+    ./elife.cls \
+    ./glossaries.tex \
+    ./bibliography.bib \
+    ./PaperFigures/elifeFigure1/figure_validation_178mm.pdf \
+    ./PaperFigures/elifeFigure2/figure_effect_of_tolerace_140mm.pdf \
+    ./PaperFigures/elifeFigure3/figure_turnover_tolerance_114.pdf \
+    ./PaperFigures/elifeFigure4/figure_camkii_activation_130mm.pdf \
+    ./PaperFigures/elifeFigure5/figure_sync_114mm.pdf \
+    ./PaperFigures/elifeFigure6/figure_two_timecourses_114mm.pdf \
+    ./PaperFigures/elifeFigure7/figure_exchange_rate.pdf \
+    ./PaperFigures/elifeFigure8/figure_su_long_term_effect.pdf \
+    ./PaperFigures/suppl/*.pdf 
+    
 ls -lh $OUTFILE
