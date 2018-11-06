618c3dba6b812483711c677d385b6451feff45df
Dilawar Singh <dilawars@ncbs.res.in>
2018-11-06 13:34:40 +0530

    Updated to script creating upload zip file.

--- create_zip_without_data_files.sh
+++ create_zip_without_data_files.sh
@@ -10,15 +10,7 @@ zip -r $OUTFILE \
     ./elife.cls \
     ./glossaries.tex \
     ./bibliography.bib \
-    ./PaperFigures/elifeFigure1/figure_validation_178mm.pdf \
-    ./PaperFigures/elifeFigure2/figure_effect_of_tolerace_140mm.pdf \
-    ./PaperFigures/elifeFigure3/figure_turnover_tolerance_114.pdf \
-    ./PaperFigures/elifeFigure4/figure_camkii_activation_130mm.pdf \
-    ./PaperFigures/elifeFigure5/figure_sync_114mm.pdf \
-    ./PaperFigures/elifeFigure6/figure_two_timecourses_114mm.pdf \
-    ./PaperFigures/elifeFigure7/figure_exchange_rate.pdf \
-    ./PaperFigures/elifeFigure8/figure_su_long_term_effect.pdf \
-    ./PaperFigures/suppl/*.pdf  \
+    ./elifeFigure*.pdf \
     ./PaperFigures/Makefile
     
 ls -lh $OUTFILE
